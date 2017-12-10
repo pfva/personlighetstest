39f8dc29e3875332b34b16bf974fa258d2e287d5
Petter <petter.f.andersson@gmail.com>
2017-12-10 11:44:31 +0100

    Button now appends new content to the DOM on click

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,9 @@
 $(document).on("click", "#instructions-button", function(){
-	$('.card-body').fadeOut("slow");
+	$('.card-body').fadeOut("slow", function(){
+		$('.card-content').append(`
+			<div class="card-body">
+				<h4 class="card-title">Test</h4>
+				<p class="card-text">Nu börjar personlighetstestet.</p>
+			</div>`);
+	});
 });
