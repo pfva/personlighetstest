f59bb627f56235d821d1b0ae0b0e86520a89ec02
Petter <petter.f.andersson@gmail.com>
2017-12-10 15:25:19 +0100

    Added template for test questions/choices and slider

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,5 @@
 $(document).on("click", "#instructions-button", function(){
-	$('.card-body').fadeOut("slow", function(){
-		$('.card-content').append(`
-			<div class="card-body">
-				<h4 class="card-title">Test</h4>
-				<p class="card-text">Nu börjar personlighetstestet.</p>
-			</div>`);
+	$('#test-instructions').fadeOut("slow", function(){
+		$('#test-questions').show();
 	});
 });
