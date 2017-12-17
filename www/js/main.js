7af06d738a411899a6d2460b1b75b7b158920493
Petter <petter.f.andersson@gmail.com>
2017-12-17 16:10:01 +0100

    Tried to render first question to the HTML, not yet working

--- www/js/main.js
+++ www/js/main.js
@@ -3,3 +3,15 @@ $(document).on("click", "#instructions-button", function(){
 		$('#test-questions').show();
 	});
 });
+
+function renderQuestions(test){
+	let that = this;
+	$.getJSON('/json/test.json', function(data){
+		that.questions = data.questions;
+	});
+
+	$('.question-start').html(`${this.questions[0].q}`);
+}
+
+renderQuestions();
+// $.getJSON('/json/test.json', renderQuestions);
