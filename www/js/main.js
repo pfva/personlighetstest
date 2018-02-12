b1dc94709a258a1613ab60bdf422e1337e99f53f
Petter <petter.f.andersson@gmail.com>
2018-02-12 21:09:38 +0100

    Final major commit (all classes added)

--- www/js/main.js
+++ www/js/main.js
@@ -1,17 +1,1 @@
-$(document).on("click", "#instructions-button", function(){
-	$('#test-instructions').fadeOut("slow", function(){
-		$('#test-questions').show();
-	});
-});
-
-/* function renderQuestions(test){
-	let that = this;
-	$.getJSON('/json/test.json', function(data){
-		that.questions = data.questions;
-	});
-
-	$('.question-start').html(`${this.questions[0].q}`);
-}
-
-renderQuestions();
-// $.getJSON('/json/test.json', renderQuestions); */
+let app = new App();
